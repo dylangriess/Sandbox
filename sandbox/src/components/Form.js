b7b41981bace7e59dd5e4f8c8706626f4318a575
dylangriess <dylangriess@gmail.com>
2022-11-28 13:45:33 -0700

    removed App.js styling

--- sandbox/src/components/Form.js
+++ sandbox/src/components/Form.js
@@ -1,31 +1,22 @@
 import React from "react";
+import { useRef } from "react";
 
 function Form() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   function onSubmit(e) {
     e.preventDefault();
-    console.log({ email, password });
+    // console.log({ email, password });
   }
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <label htmlFor="email">Email</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          id="email"
-        />
+        <input ref={emailRef} type="email" id="email" />
         <label htmlFor="password">Password</label>
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          id="password"
-        />
+        <input ref={passwordRef} type="password" id="password" />
         <button type="submit">Submit</button>
       </form>
     </div>
