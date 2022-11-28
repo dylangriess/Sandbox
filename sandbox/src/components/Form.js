8dce15c4e56901b01202ccb0414d7e82a8336aa4
dylangriess <dylangriess@gmail.com>
2022-11-28 13:30:45 -0700

    initialize form.js

---
+++ sandbox/src/components/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Form() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return <div>Form</div>;
+}
+
+export default Form;
