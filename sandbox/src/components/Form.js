3e1e21d76799619bed0bab3e3470eb1306a96eba
dylangriess <dylangriess@gmail.com>
2022-11-28 13:34:34 -0700

    form creation

--- sandbox/src/components/Form.js
+++ sandbox/src/components/Form.js
@@ -3,7 +3,33 @@ import React from "react";
 function Form() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  return <div>Form</div>;
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log({ email, password });
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          id="email"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          id="password"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
