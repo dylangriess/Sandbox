904c31d81338847a517e9dc14b5a82dd35da1acd
dylangriess <dylangriess@gmail.com>
2022-11-30 11:26:03 -0700

    counter

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Array from "./components/Array/Array";
 import Form from "./components/Form/Form";
+import Counter from "./components/Counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Form />
+        <Counter />
       </header>
     </div>
   );
---
+++ sandbox/src/components/Counter/Counter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function adjustCount(amount) {
+    setCount((currentCount) => {
+      return currentCount + amount;
+    });
+  }
+
+  return (
+    <>
+      <button onClick={() => adjustCount(-1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => adjustCount(1)}>+</button>
+    </>
+  );
+}
+
+export default Counter;
