ef519f4bea018118f670e00072a00d88de1fe8c8
dylangriess <dylangriess@gmail.com>
2022-12-17 18:33:34 -0700

    list function

--- sandbox/src/components/Todo/Todo.js
+++ sandbox/src/components/Todo/Todo.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function Todo() {
-  return <div>Todo</div>;
+const listsContainer = document.querySelector("[data-lists]");
+let lists = ["name", "todo"];
+
+function render() {
+    <li class="list-name">Work</li>
+  clearElement(listsContainer);
+  lists.forEach(list => {
+    const listElement= document.createElement("li")
+    listElement.classList.add("list-name")
+    listElement.innerText = list
+    listsContainer.appendChild(listElement)
+    
+  })
 }
+
+function clearElement(element)
 
 export default Todo;
