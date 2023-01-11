a6acadb4f0c64eded56a0ea8efbe6f9d700a4dad
dylangriess <dylangriess@gmail.com>
2023-01-10 21:41:46 -0700

    square, squareRoot, pow, log, exp

--- sandbox/src/components/Calculator/Calculator.js
+++ sandbox/src/components/Calculator/Calculator.js
@@ -3,4 +3,9 @@ const calculator = {
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => a / b,
+  square: (a) => a * a,
+  squareRoot: (a) => Math.sqrt(a),
+  pow: (a, b) => Math.pow(a, b),
+  log: (a) => Math.log10(a),
+  exp: (a) => Math.exp(a),
 };
