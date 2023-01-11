e0b52c499462bf715da2c2396acc8d07602ba4eb
dylangriess <dylangriess@gmail.com>
2023-01-10 21:43:51 -0700

    console logs testing expressions

--- sandbox/src/components/Calculator/Calculator.js
+++ sandbox/src/components/Calculator/Calculator.js
@@ -21,3 +21,19 @@ const calculator = {
     }
   },
 };
+
+console.log(calculator.add(2, 3)); // Output: 5
+console.log(calculator.subtract(2, 3)); // Output: -1
+console.log(calculator.multiply(2, 3)); // Output: 6
+console.log(calculator.divide(2, 3)); // Output: 0.6666666666666666
+console.log(calculator.square(2)); // Output: 4
+console.log(calculator.squareRoot(4)); // Output: 2
+console.log(calculator.pow(2, 3)); // Output: 8
+console.log(calculator.log(100)); // Output: 2
+console.log(calculator.exp(1)); // Output: 2.718281828459045
+console.log(calculator.sin(Math.PI / 2)); // Output: 1
+console.log(calculator.cos(0)); // Output: 1
+console.log(calculator.tan(0)); // Output: 0
+console.log(calculator.pi()); // Output: 3.141592653589793
+console.log(calculator.e()); // Output: 2.718281828459045
+console.log(calculator.solveExpression("2+3*4-1")); // Output: 11
