38b107cee6dfa19be0ea774a64d32e0f7daf2b99
dylangriess <dylangriess@gmail.com>
2023-01-11 21:18:47 -0700

    setTime with 1000 interval

--- sandbox/src/components/Clock/Clock.js
+++ sandbox/src/components/Clock/Clock.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 }
+
 function Clock() {
   return <div>Clock</div>;
 }
