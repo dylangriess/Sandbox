8e81a65e986443eed00f051970edeb8886418c70
dylangriess <dylangriess@gmail.com>
2023-01-11 21:19:09 -0700

    return div with time

--- sandbox/src/components/Clock/Clock.js
+++ sandbox/src/components/Clock/Clock.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
+
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
     }, 1000);
     return () => clearInterval(interval);
   }, []);
-}
 
-function Clock() {
-  return <div>Clock</div>;
+  return (
+    <div>
+      <h1>{time}</h1>
+    </div>
+  );
 }
 
 export default Clock;
