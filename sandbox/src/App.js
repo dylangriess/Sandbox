06f902951d970c0419004c7f2e554097fc4970be
dylangriess <dylangriess@gmail.com>
2023-01-12 21:27:28 -0700

    import quiz, render to page

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Array from "./components/Array/Array";
 import Form from "./components/Form/Form";
 import Counter from "./components/Counter/Counter";
+import Quiz from "./components/Quiz/Quiz";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <Form />
         <Counter />
+        <Quiz />
       </header>
     </div>
   );
