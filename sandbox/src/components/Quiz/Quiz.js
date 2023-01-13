a9e623a272b604e9f1952179faebbeebe8ceb26a
dylangriess <dylangriess@gmail.com>
2023-01-12 21:23:48 -0700

    added quiz questions

--- sandbox/src/components/Quiz/Quiz.js
+++ sandbox/src/components/Quiz/Quiz.js
@@ -1,4 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+const questions = [
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "Rome", "Madrid", "Berlin"],
+    answer: "Paris",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    options: ["Earth", "Jupiter", "Saturn", "Mars"],
+    answer: "Jupiter",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    options: [
+      "Leonardo da Vinci",
+      "Vincent van Gogh",
+      "Pablo Picasso",
+      "Rembrandt",
+    ],
+    answer: "Leonardo da Vinci",
+  },
+];
 
 function Quiz() {
   return <div>Quiz</div>;
